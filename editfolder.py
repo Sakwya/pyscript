4e52007a477a45be38c5b5b4532011120d356a2f
Skuyazlu <celiasash@foxmail.com>
2022-12-13 14:53:50 +0800

    go!

--- editfolder.py
+++ editfolder.py
@@ -8,35 +8,34 @@ from tkinter import simpledialog
 
 
 class FolderCopier:
-    def info(self):
-        self.__
-        return """怪猎涩涩装备复制用脚本"""
-
-    def __addFolder(self, p_dic, f_path):
-        f_plist = os.listdir(f_path)
-        for f_p in f_plist:
-            f_p = f_path + '/' + f_p
-            if os.path.isdir(f_p):
-                self.__addFolder(p_dic, f_p)
-            else:
-                p_dic.append(f_p)
-
-    def __createFolder(self, f_path):
-        if not os.path.exists(f_path[:f_path.rindex('/')]):
-            print(f_path[:f_path.rindex('/')])
-            self.__createFolder(f_path[:f_path.rindex('/')])
-        os.mkdir(f_path)
 
     def __init__(self):
         root = tk.Tk()
         root.withdraw()
+
+    info = '怪猎涩涩装备复制用脚本'
+
+    def __add_folder(self, p_dic, f_path):
+        f_plist = os.listdir(f_path)
+        for f_p in f_plist:
+            f_p = f_path + '/' + f_p
+            if os.path.isdir(f_p):
+                self.__add_folder(p_dic, f_p)
+            else:
+                p_dic.append(f_p)
+
+    def __create_folder(self, f_path):
+        if not os.path.exists(f_path[:f_path.rindex('/')]):
+            print(f_path[:f_path.rindex('/')])
+            self.__create_folder(f_path[:f_path.rindex('/')])
+        os.mkdir(f_path)
 
     def copy_folder(self):
         messagebox.showinfo("", "请选择目标文件夹")
         target_path = filedialog.askdirectory()
         path_dic = []
         new_dic = []
-        self.__addFolder(path_dic, target_path)
+        self.__add_folder(path_dic, target_path)
         origin = simpledialog.askfloat(title='', prompt='输入原装备id')
         while origin != int(origin) or origin < 1 or origin > 501:
             print(origin)
@@ -60,7 +59,7 @@ class FolderCopier:
                 print(str(i).zfill(2) + " " + path_dic[i])
                 print("   " + new_dic[i])
                 if not os.path.exists(new_dic[i][:new_dic[i].rindex("/")]):
-                    self.__createFolder(new_dic[i][:new_dic[i].rindex("/")])
+                    self.__create_folder(new_dic[i][:new_dic[i].rindex("/")])
                 shutil.copy(path_dic[i], new_dic[i])
         else:
             print(origin)
@@ -75,7 +74,5 @@ if __name__ == "__main__":
     copier = FolderCopier()
     copier.copy_folder()
     '''
-    a = FolderCopier
-    print(a.__doc__)
-    print(a.__dict__)
-    print(a.__name__)
+    a = FolderCopier()
+    print(a.info)
