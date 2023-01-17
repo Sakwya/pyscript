9814f5ec2c7b0eab5742763627a970aca578fe78
Skuyazlu <celiasash@foxmail.com>
2023-01-17 20:48:03 +0800

    tiny step

--- pyscript_list_generator.py
+++ pyscript_list_generator.py
@@ -1,31 +1,38 @@
 """用于产生pyscript的目录内容"""
 import os
+from os import path as op
 
 
 class Generator:
     path = ""
     pyscript_list = []
+    pyscript_dic = {}
 
     def __generator_pyscript_list(self):
-        path = os.getcwd()
+        path = op.join(os.getcwd(), "pyscripts")
         self.path = path
+        print(self.path)
         file_list = os.listdir(path)
         pyscript_list = []
         for file in file_list:
-            if (file.split('.')[-1]) == "py":
-                pyscript_list.append("".join(file.split('.')[0:-1]))
-
-        pyscript_list.remove("pyscript_list_generator")
-        pyscript_list.remove("__init__")
-        pyscript_list.remove("server")
+            if op.isdir(op.join(path, file)):
+                pyscript_list.append(file)
         self.pyscript_list = pyscript_list
+        self.pyscript_dic
 
     def __init__(self):
         self.__generator_pyscript_list()
 
     def generator_pyscript(self, pyscript):
-
-        return None
+        path = op.join(self.path, pyscript)
+        if not op.isdir(path):
+            return 'EOF'
+        file_list = os.listdir(path)
+        script_list =[]
+        for file in file_list:
+            if file.split('.')[-1]=='py':
+                script_list.append(file)
+        return script_list
 
     '''==='''
 
